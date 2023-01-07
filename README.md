b96bae2e2a70f47331f3dd84f16cb362a0fe2316
dtpfl <daniel.toepfl@squeezey.eu>
2023-01-07 19:05:47 +0100

    todo Behandeln ungültiger Eingaben

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 
 https://rust-lang-de.github.io
+
+# Tools
+
+## MacOS
+
+[Helix (Editor)](https://docs.helix-editor.com)
+[rust-analizer](https://rust-analyzer.github.io)
--- rust/kapitel_zwei/guessing_game/src/main.rs
+++ rust/kapitel_zwei/guessing_game/src/main.rs
@@ -1,20 +1,33 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Rate die Zahl!");
-
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    
+
     println!("Die Geheimzahl ist: {secret_number}");
-    
-    println!("Bitte gib deine Schätzung ein.");
 
-    let mut guess = String::new();
+    loop {
+        println!("Bitte gib deine Schätzung ein.");
 
-    io::stdin() 
-        .read_line(&mut guess)
-        .expect("Fehler beim Lesen der Zeile");
-    
-    println!("Du hast geschätzt: {guess}");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Fehler beim Lesen der Zeile");
+
+        let guess: u32 = guess.trim().parse().expect("Bitte gib eine Zahl ein!");
+
+        println!("Du hast geschätzt: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Zu klein!"),
+            Ordering::Greater => println!("Zu groß!"),
+            Ordering::Equal => {
+                println!("Du hast gewonnen!");
+                break;
+            }
+        }
+    }
 }
